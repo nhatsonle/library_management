edcb763e3d48d29481253f7b482ffa5bda80fd1a
nhatsonle <130040168+nhatsonle@users.noreply.github.com>
2024-08-01 00:03:26 +0700

    Add files via upload
    sql file to set up database 6

---
+++ staff.sql
@@ -0,0 +1,27 @@
+create table staff (
+	staff_id INT,
+	name VARCHAR(50),
+	phone_number VARCHAR(50),
+	mail VARCHAR(50),
+	password VARCHAR(50)
+);
+insert into staff (staff_id, name, phone_number, mail, password) values (1, 'Carleton Kehoe', '849-271-7169', 'ckehoe0@i2i.jp', 'uF3&/9=yphOn');
+insert into staff (staff_id, name, phone_number, mail, password) values (2, 'Hildegarde Gee', '337-601-9373', 'hgee1@cnbc.com', 'fI0/`+|\');
+insert into staff (staff_id, name, phone_number, mail, password) values (3, 'Julissa Jozsef', '166-572-9452', 'jjozsef2@cbslocal.com', 'zD1~tk,m');
+insert into staff (staff_id, name, phone_number, mail, password) values (4, 'Jourdain MacDiarmid', '439-152-1972', 'jmacdiarmid3@joomla.org', 'hN1{_NdXW');
+insert into staff (staff_id, name, phone_number, mail, password) values (5, 'Khalil Esh', '310-375-7726', 'kesh4@telegraph.co.uk', 'lE2+RaG>"y?');
+insert into staff (staff_id, name, phone_number, mail, password) values (6, 'Meir Cassely', '613-188-8986', 'mcassely5@51.la', 'iO4"?&4K&EE');
+insert into staff (staff_id, name, phone_number, mail, password) values (7, 'Bee Halbeard', '834-429-7664', 'bhalbeard6@ed.gov', 'xS1%284''');
+insert into staff (staff_id, name, phone_number, mail, password) values (8, 'Hastings Prickett', '611-106-7202', 'hprickett7@hostgator.com', 'bO8.GRfi}\tn');
+insert into staff (staff_id, name, phone_number, mail, password) values (9, 'Meggi Berks', '917-963-3729', 'mberks8@virginia.edu', 'kC9{0"\pICTBk+');
+insert into staff (staff_id, name, phone_number, mail, password) values (10, 'Tasha Iacopetti', '909-236-4862', 'tiacopetti9@bbc.co.uk', 'wL6,2jIbI5yL');
+insert into staff (staff_id, name, phone_number, mail, password) values (11, 'Cassandry Pavinese', '926-377-6529', 'cpavinesea@purevolume.com', 'oO6_~d(=()');
+insert into staff (staff_id, name, phone_number, mail, password) values (12, 'Bartholomew Attwoull', '385-882-8948', 'battwoullb@seattletimes.com', 'bA5<D5O,8|Iou$S#');
+insert into staff (staff_id, name, phone_number, mail, password) values (13, 'Harris Sweetenham', '706-972-0055', 'hsweetenhamc@princeton.edu', 'uA2''E2TYMH');
+insert into staff (staff_id, name, phone_number, mail, password) values (14, 'Caterina Esel', '756-923-5867', 'ceseld@weebly.com', 'hZ2#C,A)');
+insert into staff (staff_id, name, phone_number, mail, password) values (15, 'Pearce Redborn', '793-148-3220', 'predborne@creativecommons.org', 'mJ6+@&VWFcHgz%n');
+insert into staff (staff_id, name, phone_number, mail, password) values (16, 'Luther Grimbleby', '948-670-9031', 'lgrimblebyf@dedecms.com', 'lE7<wU{\m');
+insert into staff (staff_id, name, phone_number, mail, password) values (17, 'Marco Palek', '756-352-2621', 'mpalekg@fastcompany.com', 'uL3''n!O6WAt');
+insert into staff (staff_id, name, phone_number, mail, password) values (18, 'Joya Pelzer', '305-761-9308', 'jpelzerh@stumbleupon.com', 'yR5!k5.n');
+insert into staff (staff_id, name, phone_number, mail, password) values (19, 'Gwendolin Cobbold', '931-642-1652', 'gcobboldi@about.me', 'vU7,9|l32U$l"vDi');
+insert into staff (staff_id, name, phone_number, mail, password) values (20, 'Laina Pina', '833-654-6788', 'lpinaj@nature.com', 'nC9<kW!UsQ2');
